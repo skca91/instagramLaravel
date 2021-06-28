07e1c153c3285370f07afa98d0ec84cb8d4c64d4
Stephanie Correa <stephanie.correa24@gmail.com>
2021-06-28 17:35:58 -0300

    autenticacion breeze
    agregada la autenticacion con breeze

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'lastname',
+        'nick',
         'email',
         'password',
     ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('rol');
             $table->string('name');
-            $table->string('surname');
+            $table->string('lastname');
             $table->string('nick');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
