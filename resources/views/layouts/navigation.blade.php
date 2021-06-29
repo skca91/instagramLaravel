e800aea1290763c935facb86cf45b84c45cfceb1
Stephanie Correa <stephanie.correa24@gmail.com>
2021-06-29 19:59:27 -0300

    controlador y vista de usuario

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,7 +27,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('')">
+                    <x-nav-link :href="route('config')" :active="request()->routeIs('')">
                         {{ __('Settings') }}
                     </x-nav-link>
                 </div>
---
+++ resources/views/user/config.blade.php
@@ -0,0 +1,1 @@
+<h1>Configuracion de mi cuenta</h1>
